2c825aa02ffac815ef1ab1f9edca9fbc9ed14894
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-04-07 18:46:26 +0000

    starting fresh tomorrow

--- script.js
+++ script.js
@@ -1,35 +1,5 @@
 class Disc {
-    constructor(speed, weight, name) {
-        this.speed = speed,
-        this.weight = weight,
-        this.name = name
+    constructor() {
+        
     }
-
-        get speed() {
-         return this._speed;   
-        }
-
-        set speed(newSpeed) {
-            if (newSpeed > 0 && typeof newSpeed === 'number') {
-              this._speed = newSpeed;
-            } else {
-             console.error('speed must be a positive number larger than 0.') 
-            }
-        }
 }
-
-class Driver extends Disc {
-    constructor(speed, weight, name, stability, plastic) {
-        super(speed, weight, name);
-        this.stability = stability,
-        this.plastic = plastic;
-    }
-
-}
-
-const destroyer = new Disc(12, 175, 'destroyer');
-console.log(destroyer.speed);
-
-const starDestroyer = new Driver(12, 175,'destroyer', 'over', 'star');
-starDestroyer.weight = 15;
-console.log(starDestroyer.weight);
