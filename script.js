88427bba6974b66a97fca79372d673d8f8b3ba65
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-04-07 19:32:38 +0000

    made new class of vehicle. set getter set setter

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
-class Disc {
-    constructor() {
-        
+class Vehicle {
+    constructor(make, model, year) {
+        this.make = make,
+        this.model = model,
+        this.year = year,
+        this._mileage = 0
+    }
+
+    //make a setter
+    set mileage(newMileage) {
+       if (newMileage >= 0) {
+        this._mileage = newMileage;
+       } else {
+        console.error('Mileage must be greater than 0');
+       }
+    }
+
+    //make a getter
+    get mileage(){
+        return this._mileage;
+    }
+
+    //set a static method
+
+    runs() {
+        console.log('This vehicle is running');
+    }
+
+    brakes() {
+        console.log('this vehicle is stopping');
+    }
+
+    speed(mph) {
+        console.log(`this vehicle is traveling at ${mph}mph`);
     }
 }
+
+//make a new class instance that extends car with super
